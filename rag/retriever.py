85369dab7f3a2392bcbb6e40c89bfab87f8da3df
Druid <tvs9by@virginia.edu>
2026-04-01 14:52:08 -0400

    update: some errors

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,6 +1,6 @@
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
 
